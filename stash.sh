286b6f52eb05cfaa7ff412a0eba8761f248fd351
Danijel Wynyard <danijel.wynyard@mrisoftware.com>
2022-10-13 17:08:00 +1100

    2022-10-13

---
+++ stash.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+date=$(date '+%Y-%m-%d')
+docker compose down 
+rm -rf .jekyll-cache/ _site/
+git add .
+git commit -m  "$date"
+rsync -av --progress . /mnt/c/Users/Danijel.Wynyard/OneDrive\ -\ MRI\ Software/mri-docs/ --exclude .git --exclude _site --exclude .jekyll-cache
