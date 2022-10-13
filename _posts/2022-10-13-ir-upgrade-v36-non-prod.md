c6c8a36bd695e4e20d5ec9867e1e7e3d5dcb5e24
Danijel Wynyard <danijel.wynyard@mrisoftware.com>
2022-10-13 17:03:37 +1100

    20221013

---
+++ _posts/2022-10-13-ir-upgrade-v36-non-prod.md
@@ -0,0 +1,44 @@
+---
+title: IR Upgrade v36 Non-Prod
+author: danijel
+date: 2022-01-01 11:00:00 +1000
+categories: [Process]
+tags: [ir, upgrade, non-prod]
+mermaid: true
+---
+
+Upgrade process for Inland Rail v36.
+
+## Pre-Requisites
+
+### VPN
+
+Using OpenVPN, connect to VPN1 `13.239.11.251`.
+
+![VPN Connection](/assets/img/ir-upgrade-v36-non-prod/vpn%20logon.png)
+_VPN1 Connection_
+
+### PowerShell Profile
+
+Update PowerShell profile to include required custom functions:
+
+ - Get-SqlEnv
+
+Verify the PowerShell Profile exists by the following command, else add the data required.
+
+```powershell
+if (-not (Test-Path -Path $profile))
+{
+    mkdir -p $(Split-Path -Path $profile -Parent)
+    New-Item -Path $profile -ItemType File -Confirm:$false -Force
+    notepad $profile
+}
+```
+
+Verify the `$env:KEY` value is input to the profile too. This can be loaded manually per instance of PowerShell to prevent secrets being captured.
+
+### Servers
+
+| Server | IP | VPN | Description |
+|:-------|:---|:----|:------------|
+| NONPROD\SYDA-NMH-ODPa | 10.229.5.150 | VPN1 | Oracle Data Pump (Server A) |
---
+++ powershell_profile.ps1
@@ -0,0 +1,166 @@
+$Env:KEY='240 91 89 201 193 167 96 19 196 13 79 240 167 234 86 201 167 115 109 145 165 129 73 3 102 188 236 225 70 176 74 159'
+
+$Env:AWS_CLI_AUTO_PROMPT='off'
+$Env:AWS_PROFILELOCATION="$Home\.aws\credentials"
+
+$Env:AWS_CSM_ENABLED=$true
+$Env:AWS_CSM_PORT=31000
+$Env:AWS_CSM_HOST=127.0.0.1
+
+
+
+#region load AWS Profile if found
+if (Test-Path -Path $Env:AWS_PROFILELOCATION) {
+    function stc
+    {
+        param($pf)
+        $Env:AWS_ACCESS_KEY_ID 
+    
+        Set-AWSCredentials -AccessKey $Env:AWS_ACCESS_KEY_ID -SecretKey $Env:AWS_SECRET_ACCESS_KEY -SessionToken $Env:AWS_SESSION_TOKEN -StoreAs $pf -ProfileLocation $Env:AWS_PROFILELOCATION
+    }
+
+    function sac
+    {
+        param($pf)
+        $Env:AWS_PROFILE=$pf
+        Set-AWSCredential -ProfileName $pf -ProfileLocation $Env:AWS_PROFILELOCATION
+        $cred=Get-AWSCredential -ProfileName $pf -ProfileLocation $Env:AWS_PROFILELOCATION
+        $Env:AWS_ACCESS_KEY_ID=$cred.GetCredentials().AccessKey
+        $Env:AWS_SECRET_ACCESS_KEY=$cred.GetCredentials().SecretKey
+        $Env:AWS_SESSION_TOKEN=$cred.GetCredentials().Token
+
+    }
+}
+else {
+    <# Action when all if and elseif conditions are false #>
+    Write-Output "AWS credentials not available"
+    Write-Output "Copy file to: $Home\.aws\credentials"
+    Write-Output "Download from AWS Console on web"
+}
+#endregion
+
+function Set-Key
+{
+    $Env:KEY = Get-Clipboard
+}
+
+function Invoke-DisplayKey
+{
+    Write-Host $Env:KEY
+}
+Set-Alias -Name dk -Value Invoke-DisplayKey
+
+function Invoke-AssignKey
+{
+    param($k)
+    $Env:KEY+=$k
+}
+Set-Alias -Name ak -Value Invoke-AssignKey
+
+
+
+
+function Invoke-EncryptStuff
+{ 
+    Param(
+        [Parameter(ValueFromPipeline)]
+        $InputObject,
+        $Key
+    )
+    BEGIN {
+        if ( ($null -ne $Env:KEY) -and ($null -eq $Key) )
+        {
+            $Key = $Env:KEY -Split ' '
+        }
+        if ($null -eq $Key)
+        {
+            $Key = (1..16)
+        }
+   }
+
+    PROCESS {
+    #write-host $Key
+        $sstr = $InputObject | ConvertTo-SecureString -AsPlainText -Force 
+        ConvertFrom-SecureString -SecureString $sstr -Key $Key
+    }
+}
+Set-Alias -Name Encrypt-Stuff -Value Invoke-EncryptStuff
+
+function Invoke-DecryptStuff
+{ 
+    Param(
+        [Parameter(ValueFromPipeline)]
+        $InputObject,
+        $Key
+    )
+    BEGIN {
+        if ( ($null -ne $Env:KEY) -and ($null -eq $Key) )
+        {
+            $Key = $Env:KEY -Split ' '
+        }
+        if ($null -eq $Key) 
+        {
+            $Key = (1..16)
+        }
+    }
+    PROCESS {
+        #"Input:{0}, {1}"  -f $inputobject,$Key
+        #Write-Host $Key
+        $SecString = $InputObject | ConvertTo-SecureString -Key $Key
+        $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecString)
+        [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+    }
+}
+Set-Alias -Name Decrypt-Stuff -Value Invoke-DecryptStuff
+
+function csql
+{
+    Write-Host -ForegroundColor Green ("Log in as {0} onto {1}" -f $Env:SCHEMA, $Env:TNS)
+    sqlplus  $Env:SCHEMA`/$Env:PASSWD`@$Env:TNS
+}
+
+function Get-SQLEnv
+{
+    Param(
+        [Parameter(ValueFromPipeline)]
+        $InputObject,
+        $c1,
+        $c2
+    )
+    BEGIN {
+        if ($null -eq $InputObject)
+        {
+                $inputObject = (Get-Content -Path $Home\vault | Decrypt-Stuff | ConvertFrom-Json )
+        }
+    }
+    PROCESS {
+        $inputobject | Where-Object tns -like ("*{0}*" -f $c1) | Where-Object schema -like ("*{0}*" -f $c2)
+    }
+}
+
+function Set-SQLEnv
+{
+    Param(
+        [Parameter(ValueFromPipeline)]
+        $InputObject
+    )
+    BEGIN {
+
+    }
+    PROCESS {
+        $Env:TNS = $InputObject.tns
+        $Env:SCHEMA = $InputObject.schema
+        $Env:PASSWD = $InputObject.passwd
+    }
+}
+
+function Test-Tcp
+{
+    param($1,$2);
+    try {
+        $null = New-Object System.Net.Sockets.TCPClient -ArgumentList $1,$2;
+        $true;
+    } catch {
+        $false;
+    }
+}
